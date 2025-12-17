141451ad60703b2af034023e06cfc91a0d18a926
mariaasghar123 <mariaasghar74@gmail.com>
2025-12-17 16:07:34 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule,TypeOrmModule.forRoot({
+  imports: [
+    // 🔹 ENV variables load
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // 🔹 TypeORM via DATABASE_URL
+    TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'fixit_user',
-      password: 'fixit@123',
-      database: 'fixit_db',
+      url: process.env.DATABASE_URL, // ✅ MAIN FIX
       autoLoadEntities: true,
       synchronize: true, // ⚠️ dev only
-    }),],
+
+      ssl: process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
+    }),
+
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
